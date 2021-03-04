015e36fd1f92c202e582c6d8c1cb729d508d17dd
younghag <78770231+younghag@users.noreply.github.com>
2021-03-04 17:55:53 +0900

    Add files via upload

---
+++ lecture04.js
@@ -0,0 +1,2 @@
+var msg = "출력될 문자입니다."
+console.log(msg);
---
+++ lecture06.js
@@ -0,0 +1,6 @@
+var height = prompt("키를 입력해 주셔요. ")
+
+var height_int = parseInt(height);
+console.log(height_int, typeof(height_int));
+var height_float = parseFloat(height);
+console.log(height_float, typeof(height_float));
