b5ad9bdec33ddc3ac5529b3bafee7f4b6df200bc
younghag <78770231+younghag@users.noreply.github.com>
2021-04-15 17:47:55 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,245 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+
+    // # 3
+    // System.out.print("첫 자바 프로그램");
+    // System.out.print("화면에출력");
+    // # 4  2가지 방법
+    // System.out.println("홍");
+    // System.out.println("길");
+    // System.out.println("동");
+    // System.out.println("홍\n길\n동");
+    // System.out.println("홍\n\n길\n동"); 
+    // # 2-1 다양한 방법    
+  
+  // System.out.println("82 + 17 = " + (82+17));
+  // System.out.println("82 - 17 = " + (82-17));
+  // int x = 63;
+  // int y = 18;
+  // int z = 52;
+  // int sum = x+y+z;
+  // System.out.println("x값은 "+x+" 입니다.");
+  // System.out.println("y값은 "+y+" 입니다.");
+  // System.out.println("z값은 "+z+" 입니다.");
+  // System.out.println("합계는 "+ sum+" 입니다.");
+  // System.out.println("평균은 " +sum/3 + " 입니다.");
+  // Scanner sc = new Scanner(System.in);
+
+  // System.out.print("정수값: ");
+  // int x = sc.nextInt();
+  // System.out.println(x+ "를 입력했습니다.");
+  // Scanner sc= new Scanner(System.in);
+  // System.out.print("정수값: ");
+  // int x = sc.nextInt();
+  // System.out.println("10을 더한 값은 " + (x+10) + " 입니다.");
+  // System.out.println("10을 뺀 값은 " + (x-10) +"입니다.");
+  // Scanner sc = new Scanner(System.in);
+  // System.out.print("정수값: ");
+  // int x = sc.nextInt();
+  // System.out.println("마지막 자릿수를 제외한 값은" + (x/10)+"입니다.");
+  // System.out.println("마지막 자릿수는 "+(x%10)+"입니다.");
+  // Scanner sc = new Scanner(System.in);
+  // System.out.print("밑변:");
+  // double x = sc.nextDouble();
+  // System.out.print("높이:");
+  // double y = sc.nextDouble();
+  // System.out.println("넓이는 "+(x*y)/2+" 입니다.");
+  // Scanner sc = new Scanner(System.in);
+  // System.out.print("구의 겉넓이와 부피를 구합니다. ");
+  // System.out.print("반지름");
+  // Double x = sc.nextDouble();
+  // final double PI = 3.14;
+  //   System.out.println("겉넓이는 "+(4*x*x*PI)+"입니다.");
+  //   System.out.println("부피는"+(4/3*PI*x*x*x)+"입니다.");
+    // Random rand = new Random();
+    // int x = 1 + rand.nextInt(9);
+    // int y = -1 -rand.nextInt(9);
+    // int z = rand.nextInt(90)+10;
+    // System.out.print("3개의 난수를 생성했습니다.");
+    // System.out.println("한 자리의 양의 정수:" + x);
+    // System.out.println("한 자리의 음의 정수:" + y);
+    // System.out.println("두 자리의 양의 정수:" + z);
+  // import java.util.Scanner;
+  // import java.util.Random;
+  // Random rand = new Random();
+  // Scanner sc = new Scanner(System.in);
+  // System.out.print("정수값:");
+  // int x = sc.nextInt();
+
+  // System.out.println("입력값의 5의 범위의 난수를 생성했습니다.");
+  // System.out.println("값은 "+(x-5 +sc.nextInt(11))+"입니다.");
+  // Scanner sc = new Scanner(System.in);
+  // System.out.print("성: ");
+  // String x = sc.next();
+  // System.out.print("이름: ");
+  // String y = sc.next();
+
+  // System.out.println("안녕하세요." + x + y + " 씨");
+  // String s1 = "ABC";
+  // String s2 = "XYZ";
+
+  // System.out.println("문자열 s1은 " +s1+ "입니다.");
+  // System.out.println("문자열 s2는 " +s2+ "입니다.");
+
+  // s1 = "FBI";
+
+  // System.out.println("문자열 s1은 " +s1+ "입니다.");
+  // System.out.println("문자열 s2는 " +s2+ "입니다.");
+  // Scanner sc = new Scanner(System.in);
+
+  // System.out.println("정수값 : ");
+  // int n = sc.nextInt();
+
+  // if (n < 0) {
+  //   System.out.println("절대값은 " + (-n)+"입니다.");
+  // }else{ 
+  //   System.out.println("절대값은 " + (n)+"입니다.");
+  // }
+  // Scanner sc = new Scanner(System.in);
+  // int x = sc.nextInt();
+  // int y = sc.nextInt();
+  // if (x%y==0){
+  //   System.out.println("B는 A의 약수입니다.");
+  // } else {
+  //   System.out.println("B는 A의 약수가 아닙니다.");
+  // }
+// Scanner sc = new Scanner(System.in);
+
+// System.out.println("정수값 : ");
+// int n = sc.nextInt();
+
+// if (n<0) {
+//   System.out.println("이 값은 음의 값입니다. ");
+// }else if(n==0) {
+//   System.out.println("값이 0입니다. ");
+//  }
+//  else {
+//    System.out.println("이 값은 양의 값입니다.");
+//   }
+// Scanner sc = new Scanner(System.in);
+
+// System.out.print("정수값 : ");
+// int a = sc.nextInt();
+// System.out.print("정수값 : ");
+// int b = sc.nextInt();
+
+
+// if (a < b) {
+//   System.out.println("a가 크다. ");
+// }else if(b<a) {
+//   System.out.println("b가 크다. ");
+//  }
+//  else {
+//    System.out.println("a와 b가 같다.");
+//   }
+//3-9
+// Scanner sc = new Scanner(System.in);
+
+// System.out.println("정수값 : ");
+// int a = sc.nextInt();
+
+// if (a<0) {
+//   System.out.println("음의 정수값을 입력했습니다. ");
+// }else if(a % 10 == 0) {
+//   System.out.println("이 값은 10의 배수입니다. ");
+//  }
+//  else {
+//    System.out.println("이 값은 10의 배수가 아닙니다. ");
+//   }
+//3-11
+// Scanner sc = new Scanner(System.in);
+
+// System.out.println("점수 : ");
+// int a = sc.nextInt();
+
+// if (a > 100) {
+//   System.out.println("잘못된 점수입니다.");
+// }else if(a >= 90) {
+//   System.out.println("수");
+//  }else if(a >= 80) {
+//   System.out.println("우");
+//   }else if(a >= 70) {
+//   System.out.println("미");
+//   }else if(a >= 60) {
+//   System.out.println("양");
+//  } else {
+//    System.out.println("가");
+//  }
+//3-13
+// Scanner sc = new Scanner(System.in);
+// System.out.print("정수 a : ");
+// int a = sc.nextInt();
+// System.out.print("정수 b : ");
+// int b = sc.nextInt();
+
+// if(a>b){
+//   System.out.println("두 값의 차는"+ (a-b)+ "입니다.");
+// }else{
+//   System.out.println("두 값의 차는" + (b-a)+"입니다.");
+// }
+// 3-13
+// Scanner sc = new Scanner(System.in);
+// System.out.print("정수 a : ");
+// int a = sc.nextInt();
+// System.out.print("정수 b : ");
+// int b = sc.nextInt();
+// int minus;
+// minus = (a>b) ? a-b : b-a;
+// System.out.println("두 갑의 차는 " + minus +"입니다.");
+
+// Scanner sc = new Scanner(System.in);
+// System.out.print("정수 a : ");
+// int a = sc.nextInt();
+// System.out.print("정수 b : ");
+// int b = sc.nextInt();
+// System.out.print("정수 c : ");
+// int c = sc.nextInt();
+// int min = a;
+// if (b<min) min = b;
+// if (c<min) min = c;
+// System.out.println("최솟값은" +min+ "입니다.");
+
+// Scanner sc = new Scanner(System.in);
+// System.out.print("정수 a : ");
+// int a = sc.nextInt();
+// System.out.print("정수 b : ");
+// int b = sc.nextInt();
+// System.out.print("정수 c : ");
+// int c = sc.nextInt();
+// int mid;
+// if ((b >= a && c <= a) || (b <= a && c>=a))
+//   mid = a;
+// else if ((a >= b && c <= b) || (a <= b && c>=b))
+//   mid = b;
+// else
+//   mid = c;
+// System.out.println("중앙값은" +mid+ "입니다.");
+
+Scanner sc = new Scanner(System.in);
+System.out.print("정수 a : ");
+int a = sc.nextInt();
+System.out.print("정수 b : ");
+int b = sc.nextInt();
+System.out.print("정수 c : ");
+int c = sc.nextInt();
+
+System.out.println("a<=b<=c가 되도록 정렬했습니다.");
+if (a > b) {
+  int t = a;
+  a = b;
+  b = t;
+}
+if (b > c) {
+  int t = b; b = c; c = t;
+}
+if (a > b) {
+  int t = a; a = c; a = t;
+}
+  System.out.println("변수 a는 " + a + "입니다. ");
+  System.out.println("변수 b는 " + b + "입니다. ");
+  System.out.println("변수 c는 " + c + "입니다. ");
+ 
+ 
+  }
+}
